6e0970edaaeb7c530c4c4dc51943e928c557c6c6
himingway <badppg@qq.com>
2016-11-20 19:24:26 +0800

    Fix some bugs

--- Booth_Algorithm_Norm.sv
+++ Booth_Algorithm_Norm.sv
@@ -7,17 +7,17 @@ module Booth_Algorithm_Norm (
 
 	logic [32:0] w;
 
-	assign w[32:0] = (Booth_R4_input[1]^Booth_R4_input[0] === 0)?
-	    ((Booth_R4_input[2]^Booth_R4_input[0] === 0)?0:{Multiplicand[31:0], 1'b0}):
-	    ((Sign === 0)?{1'b0, Multiplicand[31:0]}:{Multiplicand[31], Multiplicand[31:0]});
+assign w[32:0] = (Booth_R4_input[1]^Booth_R4_input[0] === 0)?
+    ((Booth_R4_input[2]^Booth_R4_input[0] === 0)?0:{Multiplicand[31:0], 1'b0}):
+    ((Sign === 0)?{1'b0, Multiplicand[31:0]}:{Multiplicand[31], Multiplicand[31:0]});
 
-	genvar i;
-	generate
-	  for(i = 0;i <= 32; i = i+1) begin: Proc_inverse
-	  	assign PP[i] = w[i] ^ Booth_R4_input[2];
-	  end
-	endgenerate
+genvar i;
+generate
+    for(i = 0;i <= 32; i = i+1) begin: inverse
+        assign PP[i] = w[i]^Booth_R4_input[2];
+    end
+endgenerate
 
-	assign PP[33] = (Sign === 0)? Booth_R4_input[2]:PP[32];
+assign PP[33] = (Sign === 0)?Booth_R4_input[2]:PP[32];
 
 endmodule
--- Paraller_Multiplier.sv
+++ Paraller_Multiplier.sv
@@ -3,14 +3,13 @@ module Paraller_Multiplier (
 	input logic [31:0] Multiplicand,
 	input logic Sign,
 	output logic [63:0] Product,
-	output logic [63:0] Product1,
-	output logic [63:0] Product2
+	output logic [63:0] Product_DSP
 );
 
 	logic [33:0] PP0, PP1, PP2, PP3, PP4, PP5, PP6, PP7, 
 		PP8, PP9, PP10, PP11, PP12, PP13, PP14, PP15;
 	logic [31:0] PP16;
-	logic Error_Correction;
+	logic [15:0] Error_Correction;
 	logic [63:0] C;
 	logic [63:0] S;
 
@@ -25,9 +24,8 @@ module Paraller_Multiplier (
 		.PP9(PP9), .PP10(PP10), .PP11(PP11), .PP12(PP12), .PP13(PP13), .PP14(PP14),
 		.PP15(PP15), .PP16(PP16), .C(C), .S(S));
 
+
 	Kogge_Stone_Adder adder (.A({C[62:0],1'b0}), .B(S[63:0]), .Ci(1'b0), .S(Product));	
 
-	assign Product1 = Multiplier * Multiplicand;
-	
-	assign Product2 = {C[62:0],1'b0} + S[63:0];
+	assign Product_DSP = Multiplier * Multiplicand;
 endmodule
